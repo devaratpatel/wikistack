de3f7a2923567d68c202fc170856f687e75b1c2e
Devarat Patel <devaratpatel1@gmail.com>
2021-09-30 16:28:13 -0400

    sequelize essentials: describing data complete

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const layout = require("./views/layout");
+const { db } = require("./models");
 
 app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  try {
+    res.send(layout(""));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+const PORT = 8080;
 app.listen(PORT, console.log("server is listening"));
